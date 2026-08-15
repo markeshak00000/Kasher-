4976808e120940192f52434d8ea625ad2242f1d8
markeshak00000 <markeshak00000@gmail.com>
2026-08-15 20:59:02 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: { nodeIntegration: true }
+  });
+  win.loadFile(path.join(__dirname, 'www', 'index.html'));
+}
+app.whenReady().then(createWindow);
